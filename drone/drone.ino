cadc5ccf7a780d4c25410bc9167d089019be7d7c
Malox610 <75571264+Malox610@users.noreply.github.com>
2022-10-18 18:10:49 +0200

    mmdmdm

--- drone/drone.ino
+++ drone/drone.ino
@@ -1,35 +1,42 @@
 //Include Libraries
 #include <SPI.h>
-#include <nRF24L01.h>
+
 #include <RF24.h>
 #include <Servo.h> 
 
+
+#define pinCE       9       // On associe la broche "CE" du NRF24L01 à la sortie digitale D7 de l'arduino
+#define pinCSN      8       // On associe la broche "CSN" du NRF24L01 à la sortie digitale D8 de l'arduino
+
+#define tunnel1 "PIPE2"     // On définit un premier "nom de tunnel" (5 caractères), pour pouvoir envoyer des données à l'autre NRF24
+#define tunnel2 "PIPE1"     // On définit un second "nom de tunnel" (5 caractères), pour pouvoir recevoir des données de l'autre NRF24
+
+const byte adresses[][6] = {tunnel1, tunnel2};    // Tableau des adresses de tunnel, au format "byte array"
+
+
 //create an RF24 object
-RF24 radio(9, 8);  // CE, CSN
+RF24 radio(pinCE, pinCSN);  // Instanciation du NRF24L01
 Servo myServoX1;
 Servo myServoY1;
 Servo myServoX2;
 Servo myServoY2;
 
-//address through which two modules communicate.
-const byte address[6] = "00001";
 
 void setup()
 {
   while (!Serial);
-    Serial.begin(9600);
+    Serial.begin(115200);
     myServoX1.attach(6);
     myServoY1.attach(5);
     myServoX2.attach(4);
     myServoY2.attach(3);
     
-  radio.begin();
-  
-  //set the address
-  radio.openReadingPipe(0, address);
-  
-  //Set module as receiver
-  radio.startListening();
+  radio.begin();                           // Initialisation du module NRF24
+  radio.openWritingPipe(adresses[0]);      // Ouverture du "tunnel1" en ÉCRITURE
+  radio.openReadingPipe(1, adresses[1]);   // Ouverture du "tunnel2" en LECTURE
+  radio.setPALevel(RF24_PA_MIN);           // Sélection d'un niveau "MINIMAL" pour les essais mettre RF24_PA_MAX pour les vrai test
+  radio.setChannel(96); // fréquence 2496 Mhz
+   delay(1000); 
 }
 
 void loop()
@@ -40,6 +47,7 @@ int PosX2 = 90;
 int PosY2 = 90;
 
   //Read the data if available in buffer
+  radio.startListening();
   if (radio.available())
   {
     
@@ -60,5 +68,13 @@ int PosY2 = 90;
   Serial.println(PosX1);
   Serial.println(PosY1);
   Serial.println(PosX2);
-  Serial.println(PosY2);  }
+  Serial.println(PosY2);  
+
+  }
+ int hauteur =4;
+   // ******** ENVOI ********
+  radio.stopListening();                                                  // On commence par arrêter le mode écoute, pour pouvoir émettre les données
+  radio.write(&hauteur, sizeof(hauteur)); // … et on envoi cette valeur à l'autre arduino, via le NRF24
+  delay(20); 
+  
 }
--- manette/manette.ino
+++ manette/manette.ino
@@ -1,10 +1,16 @@
 //Include Libraries
 #include <SPI.h>
-#include <nRF24L01.h>
+
 #include <RF24.h>
 
-//create an RF24 object
-RF24 radio(5, 4);  // CE, CSN
+#define pinCE     5         // On associe la broche "CE" du NRF24L01 à la sortie digitale D7 de l'arduino
+#define pinCSN    4         // On associe la broche "CSN" du NRF24L01 à la sortie digitale D8 de l'arduino
+
+#define tunnel1 "PIPE1"     // On définit un premier "nom de tunnel" (5 caractères), pour pouvoir envoyer des données à l'autre NRF24
+#define tunnel2 "PIPE2"     // On définit un second "nom de tunnel" (5 caractères), pour pouvoir recevoir des données de l'autre NRF24
+
+
+RF24 radio(pinCE, pinCSN);  // Instanciation du NRF24L01
 
 const int SW1_pin = 2; 
 const int X1_pin = A3; 
@@ -13,17 +19,21 @@ const int SW2_pin = 3;
 const int X2_pin = A1;
 const int Y2_pin = A0; 
 
-const byte address[6] = "00001";
+const byte adresses[][6] = {tunnel1, tunnel2};    // Tableau des adresses de tunnel, au format "byte array"
+
 
 void setup()
 {
-  Serial.begin(9600);
-
+    Serial.begin(115200);
   
   int p1=0;
   radio.begin();
-  radio.openWritingPipe(address);
-  radio.stopListening();
+  radio.begin();                           // Initialisation du module NRF24
+  radio.openWritingPipe(adresses[0]);      // Ouverture du "tunnel1" en ÉCRITURE
+  radio.openReadingPipe(1, adresses[1]);   // Ouverture du "tunnel2" en LECTURE
+  radio.setPALevel(RF24_PA_MIN);           // Sélection d'un niveau "MINIMAL" pour les essais mettre RF24_PA_MAX pour les vrai test
+  radio.setChannel(96); // fréquence 2496 Mhz
+  
 }
 void loop()
 {
@@ -55,7 +65,22 @@ void loop()
   Serial.println(PosY1);
   Serial.println(PosX2);
   Serial.println(PosY2);
-
-  bool rep = radio.write(Pos, sizeof(Pos));
+  radio.stopListening();     
+  radio.write(Pos, sizeof(Pos));
   delay(100);
+
+  //récéption
+ int hauteur ;
+ radio.startListening();                                                 // On commence par arrêter le mode envoi, pour pouvoir réceptionner des données
+  if(radio.available()) {                                                 // On regarde si une donnée a été reçue
+    while (radio.available()) {                                           // Si une donné est en attente de lecture, on va la lire
+      radio.read(&hauteur, sizeof(hauteur));  // Lecture des donnéwaz es reçues, une par une
+     Serial.print("Recu : ");
+     Serial.println(hauteur);
+    }
+    delay(20);                                                             // avec une petite pause, avant de reboucler
+  }
+
+
+  
 }
